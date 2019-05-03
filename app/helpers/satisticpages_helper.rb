c497dd4f437d71126f677fa330498c2fd924e5f9
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-03 11:56:46 +0900

    Created static pages

---
+++ app/helpers/satisticpages_helper.rb
@@ -0,0 +1,2 @@
+module SatisticpagesHelper
+end
---
+++ test/controllers/satisticpages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SatisticpagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
